476b826a0c5f6ba187c4e52e4cafc5830aa6e9c7
ayon_ssp <ayon.ssp@gmail.com>
2023-06-17 16:57:28 +0530

    User Login

--- src/App.js
+++ src/App.js
@@ -8,33 +8,26 @@ import Activate from './containers/Activate';
 import ResetPassword from './containers/ResetPassword';
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Layout from './hocs/Layout';
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route exact path='/login' element={<Login />} />
-                <Route exact path='/signup' element={<Signup />} />
-                <Route exact path='/reset-password' element={<ResetPassword />} />
-                <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
-                <Route exact path='/activate/:uid/:token' element={<Activate />} />
-            </Routes>
-        </Layout>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/login' element={<Login />} />
+                    <Route exact path='/signup' element={<Signup />} />
+                    <Route exact path='/reset-password' element={<ResetPassword />} />
+                    <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+                    <Route exact path='/activate/:uid/:token' element={<Activate />} />
+                </Routes>
+            </Layout>
+        </Router>
+    </Provider>
 );
-
-
-
-// working
-// const App = () => (
-//     <Router>
-//         <Routes>
-//             <Route path='/' element={<Home/>} />
-//         </Routes>
-//     </Router>
-// );
-
 
 export default App;
